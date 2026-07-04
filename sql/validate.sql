57eef4d33ea7c80dbbf8ac53dfb892adc845cc96
ParthaSB81 <parthasb81@gmail.com>
2026-07-04 13:45:29 +0530

    Validation sql

--- sql/validate.sql
+++ sql/validate.sql
@@ -157,3 +157,111 @@ from analytics.fact_payments fp
 join analytics.dim_customer dc
 on fp.customer_key = dc.customer_key
 where dc.customer_key IS NULL;
+
+
+--// Missing Product
+SELECT foi.*
+FROM analytics.fact_order_items foi
+LEFT JOIN analytics.dim_product dp
+    ON foi.product_key = dp.product_key
+WHERE dp.product_key IS NULL;
+
+
+--// Missing Seller
+SELECT foi.*
+FROM analytics.fact_order_items foi
+LEFT JOIN analytics.dim_seller ds
+    ON foi.seller_key = ds.seller_key
+WHERE ds.seller_key IS NULL;
+
+--// Negative Revenue //--
+
+-- Table : Fact Orders
+select * 
+from analytics.fact_orders
+where 
+total_order_value < 0
+    OR
+total_freight < 0
+    OR
+total_payment < 0;
+
+-- Table : Fact Order Items
+select * 
+from analytics.fact_order_items
+where
+    price < 0
+    OR
+    freight_value < 0;
+
+-- Table : Order Payments
+select *
+from analytics.fact_payments
+where
+    payment_value < 0;
+
+
+--## Impossible Delivery Days ##--
+SELECT 'Negative delivery days' AS check_name, COUNT(*) AS issue_count
+FROM analytics.fact_orders
+WHERE delivery_days < 0
+
+UNION ALL
+
+SELECT 'Delivery days > 90', COUNT(*)
+FROM analytics.fact_orders
+WHERE delivery_days > 90
+
+UNION ALL
+
+SELECT 'Extreme delay days', COUNT(*)
+FROM analytics.fact_orders
+WHERE delay_days < -365
+   OR delay_days > 365;
+
+
+--// Future Date checks //--
+SELECT 'fact_orders' AS table_name, COUNT(*) AS future_date_count
+FROM analytics.fact_orders fo
+JOIN analytics.dim_date dd
+    ON fo.date_key = dd.date_key
+WHERE dd.full_date > CAST(GETDATE() AS DATE)
+
+UNION ALL
+
+SELECT 'fact_order_items', COUNT(*)
+FROM analytics.fact_order_items foi
+JOIN analytics.dim_date dd
+    ON foi.date_key = dd.date_key
+WHERE dd.full_date > CAST(GETDATE() AS DATE)
+
+UNION ALL
+
+SELECT 'fact_payments', COUNT(*)
+FROM analytics.fact_payments fp
+JOIN analytics.dim_date dd
+    ON fp.date_key = dd.date_key
+WHERE dd.full_date > CAST(GETDATE() AS DATE);
+
+
+--// Duplicate payment key check //--
+
+WITH duplicate_payments AS
+(
+    SELECT
+        order_id,
+        payment_sequential
+    FROM analytics.fact_payments
+    GROUP BY
+        order_id,
+        payment_sequential
+    HAVING COUNT(*) > 1
+)
+SELECT fp.*
+FROM analytics.fact_payments fp
+JOIN duplicate_payments d
+    ON fp.order_id = d.order_id
+   AND fp.payment_sequential = d.payment_sequential
+ORDER BY
+    fp.order_id,
+    fp.payment_sequential;
